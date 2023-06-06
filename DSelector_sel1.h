124cc2b3edc14156fabef398b6ecd47edaa63a27
Daniel Barton <dbarton@ifarm1901.jlab.org>
2023-06-05 23:32:06 -0400

    Initial Commit

---
+++ DSelector_sel1.h
@@ -0,0 +1,80 @@
+#ifndef DSelector_sel1_h
+#define DSelector_sel1_h
+
+#include <iostream>
+
+#include "DSelector/DSelector.h"
+#include "DSelector/DHistogramActions.h"
+#include "DSelector/DCutActions.h"
+
+#include "TH1I.h" //Class
+#include "TH2I.h" //Class that does something
+
+class DSelector_sel1 : public DSelector
+{
+	public:
+
+		DSelector_sel1(TTree* locTree = NULL) : DSelector(locTree){}
+		virtual ~DSelector_sel1(){}
+
+		void Init(TTree *tree);
+		Bool_t Process(Long64_t entry);
+
+	private:
+
+		void Get_ComboWrappers(void);
+		void Finalize(void);
+
+		// BEAM POLARIZATION INFORMATION
+		UInt_t dPreviousRunNumber;
+		bool dIsPolarizedFlag; //else is AMO
+		bool dIsPARAFlag; //else is PERP or AMO
+
+		bool dIsMC;
+
+		// ANALYZE CUT ACTIONS
+		// // Automatically makes mass histograms where one cut is missing
+		DHistogramAction_AnalyzeCutActions* dAnalyzeCutActions;
+
+		//CREATE REACTION-SPECIFIC PARTICLE ARRAYS
+
+		//Step 0
+		DParticleComboStep* dStep0Wrapper;
+		DBeamParticle* dComboBeamWrapper;
+		DChargedTrackHypothesis* dPiPlus1Wrapper;
+		DChargedTrackHypothesis* dKMinusWrapper;
+		DChargedTrackHypothesis* dProtonWrapper;
+
+		//Step 1
+		DParticleComboStep* dStep1Wrapper;
+		DChargedTrackHypothesis* dPiMinusWrapper;
+		DChargedTrackHypothesis* dPiPlus2Wrapper;
+
+		// DEFINE YOUR HISTOGRAMS HERE
+		// EXAMPLES:
+		TH1I* dHist_MissingMassSquared; //* means "pointer"
+		TH1I* dHist_BeamEnergy;
+		TH1I* dHist_mass_pip_pim;
+		TH1I* dHist_IM_Pip1P;
+		TH1I* dHist_mass_ks_km; //added 5/1/2023
+		TH1I* dHist_IM_KsKm;
+
+	ClassDef(DSelector_sel1, 0);
+};
+
+void DSelector_sel1::Get_ComboWrappers(void)
+{
+	//Step 0
+	dStep0Wrapper = dComboWrapper->Get_ParticleComboStep(0);
+	dComboBeamWrapper = static_cast<DBeamParticle*>(dStep0Wrapper->Get_InitialParticle());
+	dPiPlus1Wrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(0));
+	dKMinusWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(1));
+	dProtonWrapper = static_cast<DChargedTrackHypothesis*>(dStep0Wrapper->Get_FinalParticle(3));
+
+	//Step 1
+	dStep1Wrapper = dComboWrapper->Get_ParticleComboStep(1);
+	dPiMinusWrapper = static_cast<DChargedTrackHypothesis*>(dStep1Wrapper->Get_FinalParticle(0));
+	dPiPlus2Wrapper = static_cast<DChargedTrackHypothesis*>(dStep1Wrapper->Get_FinalParticle(1));
+}
+
+#endif // DSelector_sel1_h
