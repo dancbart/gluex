068367c48ab34bffd4fda1c58e8368f530346939
Daniel Barton <dbart013@odu.edu>
2023-06-12 23:46:24 -0400

    filename change

---
+++ pippim_flat_bx2_analysis.C
@@ -0,0 +1,47 @@
+//This method used RDataFrames (instead of reading directly from TTrees) to read data from a ROOT file.
+#include <iostream>
+#include <ROOT/RDataFrame.hxx>
+#include <TH1.h>
+#include <TCanvas.h>
+
+void beamX4meas() {
+    // Open the ROOT file containing the data tree of interest
+    ROOT::RDataFrame df("pipkmks__B4_M16", "pipkmks_flat_bestX2_2017.root");
+
+    // Create a new RDataFrame object for applying column transformations
+    auto df2 = df.Define("pip2pim_E", "pip2_E + pim_E")
+                 .Define("pip2pim_px", "pip2_px + pim_px")
+                 .Define("pip2pim_py", "pip2_py + pim_py")
+                 .Define("pip2pim_pz", "pip2_pz + pim_pz")
+                 .Define("pip2pim_m", "sqrt(pip2pim_E*pip2pim_E - pip2pim_px*pip2pim_px - pip2pim_py*pip2pim_py - pip2pim_pz*pip2pim_pz)");
+
+    // Create a histogram object and fill it with the data from the column "pip2pim_m2"
+    auto h1 = df2.Histo1D({"h1", "Mass of pip2pim", 100, 0.0, 1.0}, "pip2pim_m");
+    
+    // Customize the appearance of the histogram
+    h1->SetFillColor(kBlue);
+    h1->SetLineWidth(2);
+    h1->SetFillColor(kOrange);
+    
+    // Draw the histogram on a canvas
+    TCanvas* c1 = new TCanvas("Canvas", "Mass of pip2pim", 800, 600);
+    h1->Draw();
+    c1->Update();
+    c1->SaveAs("pip2pim_m.png");
+    //this is a test
+    auto h2 = df2.Histo2D<TH2D>({"h2", "pathlength_sig vs pip2pim_m", 100, 0.3, 0.7, 100, 0.0, 10.0}, "pip2pim_m", "pathlength_sig");
+
+    h2->SetFillColor(kBlue);
+    h2->SetLineWidth(2);
+    h2->SetFillColor(kOrange);
+
+    TCanvas* c2 = new TCanvas("Canvas", "pathlength_sig vs. pip2pim_m", 800, 600);
+    h2->Draw("colz");
+    c2->Update();
+    c2->SaveAs("pathlength_sigVSpip2pim_m.png");
+}
+
+int main() {
+    beamX4meas();
+    return 0;
+}
