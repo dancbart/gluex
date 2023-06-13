e64096701b030b1e46b74931ed771efea6d9d9a7
Daniel Barton <dbart013@odu.edu>
2023-06-13 16:06:20 -0400

    create:prot+pip1 massColmns

--- pippim_flat_bx2_analysis.C
+++ pippim_flat_bx2_analysis.C
@@ -13,24 +13,30 @@ void pippim_flat_bx2_analysis() {
                  .Define("pip2pim_px", "pip2_px + pim_px")
                  .Define("pip2pim_py", "pip2_py + pim_py")
                  .Define("pip2pim_pz", "pip2_pz + pim_pz")
-                 .Define("pip2pim_m", "sqrt(pip2pim_E*pip2pim_E - pip2pim_px*pip2pim_px - pip2pim_py*pip2pim_py - pip2pim_pz*pip2pim_pz)");
+                 .Define("pip2pim_m", "sqrt(pip2pim_E*pip2pim_E - pip2pim_px*pip2pim_px - pip2pim_py*pip2pim_py - pip2pim_pz*pip2pim_pz)")
+                 .Define("pip1p_E", "pip1_E + p_E")
+                 .Define("pip1p_px", "pip1_px + p_px")
+                 .Define("pip1p_py", "pip1_py + p_py")
+                 .Define("pip1p_pz", "pip1_pz + p_pz")
+                 .Define("pip1p_m", "sqrt(pip1p_E*pip1p_E - pip1p_px*pip1p_px - pip1p_py*pip1p_py - pip1p_pz*pip1p_pz)");
 
     auto h1 = df2.Histo1D({"h1", "Mass of pip2pim", 100, 0.0, 1.0}, "pip2pim_m");
-    auto h2 = df2.Filter("pathlength_sig > 1").Histo1D({"h2", "title2", 100, 0.3, 0.75}, "pip2pim_m");
-    auto h3 = df2.Filter("pathlength_sig > 3").Histo1D({"h3", "title3", 100, 0.3, 0.75}, "pip2pim_m");
-    auto h4 = df2.Filter("pathlength_sig > 5").Histo1D({"h4", "title4", 100, 0.3, 0.75}, "pip2pim_m");
+    auto h2 = df2.Histo1D({"h2", "Mass of pip1p", 100, 0.0, 1.0}, "pip1p_m");
+    // auto h2 = df2.Filter("pathlength_sig > 1").Histo1D({"h2", "title2", 100, 0.3, 0.75}, "pip2pim_m");
+    // auto h3 = df2.Filter("pathlength_sig > 3").Histo1D({"h3", "title3", 100, 0.3, 0.75}, "pip2pim_m");
+    // auto h4 = df2.Filter("pathlength_sig > 5").Histo1D({"h4", "title4", 100, 0.3, 0.75}, "pip2pim_m");
 
-    TCanvas* c1 = new TCanvas("Canvas1", "pip2pim_flightSig", 800, 600); // Draw the histogram on a canvas
-    // h1->SetLineColor(kBlue);
-    // h1->Draw();
-    h2->SetLineColor(kBlue);
+    TCanvas* c1 = new TCanvas("Canvas1", "pip1p_m", 800, 600); // Draw the histogram on a canvas
+    h1->SetLineColor(kBlue);
+    h1->Draw();
+    h2->SetLineColor(kGreen);
     h2->Draw("SAME");
-    h3->SetLineColor(kGreen);
-    h3->Draw("SAME");
-    h4->SetLineColor(kRed);
-    h4->Draw("SAME");
+    // h3->SetLineColor(kGreen);
+    // h3->Draw("SAME");
+    // h4->SetLineColor(kRed);
+    // h4->Draw("SAME");
     c1->Update();
-    c1->SaveAs("pip2pim_m.png");
+    c1->SaveAs("pip1p_m.png");
     
 
 }
