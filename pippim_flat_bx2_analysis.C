36de9c02132a79d4dd3ee79e92fce973a60ccc2c
Daniel Barton <dbart013@odu.edu>
2023-06-13 15:09:42 -0400

    4histos->1Canvas:pathSig0;1;3;5

--- pippim_flat_bx2_analysis.C
+++ pippim_flat_bx2_analysis.C
@@ -15,28 +15,24 @@ void pippim_flat_bx2_analysis() {
                  .Define("pip2pim_pz", "pip2_pz + pim_pz")
                  .Define("pip2pim_m", "sqrt(pip2pim_E*pip2pim_E - pip2pim_px*pip2pim_px - pip2pim_py*pip2pim_py - pip2pim_pz*pip2pim_pz)");
 
-    auto fsCut1 = df2.Filter("pathlength_sig > 0 && pathlength_sig < 10");
-
-    // Create histogram: Occurences ("counts") vs. mass:
     auto h1 = df2.Histo1D({"h1", "Mass of pip2pim", 100, 0.0, 1.0}, "pip2pim_m");
-    h1->SetFillColor(kBlue); // Customize the appearance of the histogram
-    h1->SetLineWidth(2);
-    h1->SetFillColor(kOrange);
-    TCanvas* c1 = new TCanvas("Canvas1", "Mass of pip2pim", 800, 600); // Draw the histogram on a canvas
-    h1->Draw();
+    auto h2 = df2.Filter("pathlength_sig > 1").Histo1D({"h2", "title2", 100, 0.3, 0.75}, "pip2pim_m");
+    auto h3 = df2.Filter("pathlength_sig > 3").Histo1D({"h3", "title3", 100, 0.3, 0.75}, "pip2pim_m");
+    auto h4 = df2.Filter("pathlength_sig > 5").Histo1D({"h4", "title4", 100, 0.3, 0.75}, "pip2pim_m");
+
+    TCanvas* c1 = new TCanvas("Canvas1", "pip2pim_flightSig", 800, 600); // Draw the histogram on a canvas
+    // h1->SetLineColor(kBlue);
+    // h1->Draw();
+    h2->SetLineColor(kBlue);
+    h2->Draw("SAME");
+    h3->SetLineColor(kGreen);
+    h3->Draw("SAME");
+    h4->SetLineColor(kRed);
+    h4->Draw("SAME");
     c1->Update();
     c1->SaveAs("pip2pim_m.png");
     
-    // Create 2D plot: pathlength_sig (0 is low confidence, 10 is high) vs. pip2pim_m
-    // First argument is mass (x-axis), second argument is pathlength_sig (y-axis)
-    auto h2 = df2.Histo2D<TH2D>({"h2", "pathlength_sig vs pip2pim_m", 100, 0.3, 0.7, 100, 0.0, 10.0}, "pip2pim_m", "pathlength_sig");
-    h2->SetFillColor(kBlue);
-    h2->SetLineWidth(2);
-    h2->SetFillColor(kOrange);
-    TCanvas* c2 = new TCanvas("Canvas2", "pathlength_sig vs. pip2pim_m", 800, 600);
-    h2->Draw("colz");
-    c2->Update();
-    c2->SaveAs("pathlength_sigVSpip2pim_m.png");
+
 }
 
 int main() {
