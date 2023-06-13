4e1ed69553a6d38209a306a84bd7cb91aee0a9f4
Daniel Barton <dbart013@odu.edu>
2023-06-12 23:15:20 -0400

    Commit all

--- beamX4meas.C
+++ beamX4meas.C
@@ -16,22 +16,29 @@ void beamX4meas() {
                  .Define("pip2pim_m", "sqrt(pip2pim_E*pip2pim_E - pip2pim_px*pip2pim_px - pip2pim_py*pip2pim_py - pip2pim_pz*pip2pim_pz)");
 
     // Create a histogram object and fill it with the data from the column "pip2pim_m2"
-    //auto h1 = df2.Histo1D({"h1", "Mass of pip2pim", 100, 0.0, 1.0}, "pip2pim_m");
-    auto h1 = df2.Histo2D<TH2D>({"h1", "pathlength_sig vs pip2pim_m", 100, 0.0, 1.0, 100, 0.0, 1.0}, "pathlength_sig", "pip2pim_m");
-
-
+    auto h1 = df2.Histo1D({"h1", "Mass of pip2pim", 100, 0.0, 1.0}, "pip2pim_m");
+    
     // Customize the appearance of the histogram
     h1->SetFillColor(kBlue);
     h1->SetLineWidth(2);
     h1->SetFillColor(kOrange);
-    // h1->GetYaxis()->SetRangeUser(0.0, 1.0);
-
+    
     // Draw the histogram on a canvas
-    TCanvas* c1 = new TCanvas("Canvas", "Mass", 800, 600);
+    TCanvas* c1 = new TCanvas("Canvas", "Mass of pip2pim", 800, 600);
     h1->Draw();
     c1->Update();
-    c1->SaveAs("pathlength_sigVSpip2pim_m.png");
+    c1->SaveAs("pip2pim_m.png");
+    //this is a test
+    auto h2 = df2.Histo2D<TH2D>({"h2", "pathlength_sig vs pip2pim_m", 100, 0.3, 0.7, 100, 0.0, 10.0}, "pip2pim_m", "pathlength_sig");
 
+    h2->SetFillColor(kBlue);
+    h2->SetLineWidth(2);
+    h2->SetFillColor(kOrange);
+
+    TCanvas* c2 = new TCanvas("Canvas", "pathlength_sig vs. pip2pim_m", 800, 600);
+    h2->Draw("colz");
+    c2->Update();
+    c2->SaveAs("pathlength_sigVSpip2pim_m.png");
 }
 
 int main() {
--- readTTreeHeaders_anyTFile.C
+++ readTTreeHeaders_anyTFile.C
@@ -1,8 +1,9 @@
+//Use this script to read headers for any root file.  Replace 'file' & 'tree' names below.
 #include <TFile.h>
 #include <TTree.h>
 #include <iostream>
 
-void ROOT_read_headers() {
+void ROOT_readHeaders_anyFile() {
 
     std::string fileName = "pipkmks_flat_bestX2_2017.root";
     std::string treeName = "pipkmks__B4_M16";
@@ -17,6 +18,6 @@ const auto& columnNames = df.GetColumnNames();
 }
 
 int main() {
-    ROOT_read_headers();
+    ROOT_readHeaders_anyFile();
     return 0;
 }
