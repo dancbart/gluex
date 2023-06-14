2b0eac48410ac8aef2caf12ca31c3da957092330
Daniel Barton <dbart013@odu.edu>
2023-06-14 09:42:44 -0400

    newFilenm;masses:pip2pim;pip1p

--- pipkmks_flat_bx2_analysis.C
+++ pipkmks_flat_bx2_analysis.C
@@ -1,40 +1,27 @@
-//This method uses RDataFrames, instead of reading directly from ROOT file with TTrees method.
-#include <iostream>
-#include <ROOT/RDataFrame.hxx>
-#include <TH1.h>
-#include <TCanvas.h>
-
-void pippim_flat_bx2_analysis() {
+void pipkmks_flat_bx2_analysis() {
     // Open the ROOT file containing the data tree of interest
     ROOT::RDataFrame df("pipkmks__B4_M16", "pipkmks_flat_bestX2_2017.root");
 
     // Define new RDataFrame 'df2' from 'df', then add columns (using '.Define')
-    auto df2 = df.Define("pip2pim_E", "pip2_E + pim_E")
+    auto df2 = df.Define("pip2pim_E", "pip2_E + pim_E") //pi+2 and pim 4-momentum
                  .Define("pip2pim_px", "pip2_px + pim_px")
                  .Define("pip2pim_py", "pip2_py + pim_py")
                  .Define("pip2pim_pz", "pip2_pz + pim_pz")
                  .Define("pip2pim_m", "sqrt(pip2pim_E*pip2pim_E - pip2pim_px*pip2pim_px - pip2pim_py*pip2pim_py - pip2pim_pz*pip2pim_pz)")
-                 .Define("pip1p_E", "pip1_E + p_E")
+                 .Define("pip1p_E", "pip1_E + p_E")  //pi+1 and proton 4-momentum
                  .Define("pip1p_px", "pip1_px + p_px")
                  .Define("pip1p_py", "pip1_py + p_py")
                  .Define("pip1p_pz", "pip1_pz + p_pz")
                  .Define("pip1p_m", "sqrt(pip1p_E*pip1p_E - pip1p_px*pip1p_px - pip1p_py*pip1p_py - pip1p_pz*pip1p_pz)");
 
-    auto h1 = df2.Histo1D({"h1", "Mass of pip2pim", 100, 0.0, 1.0}, "pip2pim_m");
-    auto h2 = df2.Histo1D({"h2", "Mass of pip1p", 100, 0.0, 1.0}, "pip1p_m");
-    // auto h2 = df2.Filter("pathlength_sig > 1").Histo1D({"h2", "title2", 100, 0.3, 0.75}, "pip2pim_m");
-    // auto h3 = df2.Filter("pathlength_sig > 3").Histo1D({"h3", "title3", 100, 0.3, 0.75}, "pip2pim_m");
-    // auto h4 = df2.Filter("pathlength_sig > 5").Histo1D({"h4", "title4", 100, 0.3, 0.75}, "pip2pim_m");
+    auto h1 = df2.Histo1D({"h1", "pip2pim & pip1p masses", 100, 0.0, 2.0}, "pip2pim_m");
+    auto h2 = df2.Histo1D({"h2", "pip2pim & pip1p masses", 100, 0.0, 2.2}, "pip1p_m");
 
     TCanvas* c1 = new TCanvas("Canvas1", "pip1p_m", 800, 600); // Draw the histogram on a canvas
     h1->SetLineColor(kBlue);
     h1->Draw();
     h2->SetLineColor(kGreen);
     h2->Draw("SAME");
-    // h3->SetLineColor(kGreen);
-    // h3->Draw("SAME");
-    // h4->SetLineColor(kRed);
-    // h4->Draw("SAME");
     c1->Update();
     c1->SaveAs("pip1p_m.png");
     
@@ -42,6 +29,6 @@ void pippim_flat_bx2_analysis() {
 }
 
 int main() {
-    pippim_flat_bx2_analysis();
+    pipkmks_flat_bx2_analysis();
     return 0;
 }
