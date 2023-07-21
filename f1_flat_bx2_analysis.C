54310fee84f2e702994cc8a0016e04bd2cda0d2d
Daniel Barton <dbart013@odu.edu>
2023-07-21 12:19:03 -0400

    composite fxn wrks, added parameters

--- f1_flat_bx2_analysis.C
+++ f1_flat_bx2_analysis.C
@@ -1,4 +1,4 @@
-void ks_flat_bx2_fits() {
+void f1_flat_bx2_analysis() {
 
     ROOT::RDataFrame df("pipkmks__B4_M16", "pipkmks_flat_bestX2_2017.root");
 
@@ -83,23 +83,29 @@ void ks_flat_bx2_fits() {
     //auto yMax = 12500;
 
     // Function fits
-    std::unique_ptr<TF1> fitFcn1 = std::make_unique<TF1>("ks_m", "gaus", 0.3, 0.7);
+    // std::unique_ptr<TF1> fitFcn1 = std::make_unique<TF1>("fitFcn1", "gaus", 0.3, 0.7);
     // fitFcn->SetNpx(500);
     // fitFcn->SetLineWidth(2);
     // fitFcn1->SetLineColor(kMagenta);
-    h1->Fit(fitFcn1.get(), "R");
+    // h1->Fit(fitFcn1.get(), "R");
 
-    std::unique_ptr<TF1> fitFcn2 = std::make_unique<TF1>("ks_m", "pol1", 0.3, 0.7);
+    // std::unique_ptr<TF1> fitFcn2 = std::make_unique<TF1>("fitFcn2", "pol1", 0.3, 0.7);
     // fitFcn2->SetNpx(500);
     // fitFcn2->SetLineWidth(2);
     // fitFcn2->SetLineColor(kBlack);
-    h1->Fit(fitFcn2.get(), "R");
+    // h1->Fit(fitFcn2.get(), "R");
 
     // Build composite fit function
-    // std::unique_ptr<TF1> fitFcn3 = std::make_unique<TF1>("ks_m", "gaus(0) + pol1(1)", 0.3, 0.7);
-    std::unique_ptr<TF1> fitFcn3 = std::make_unique<TF1>("ks_m", "fitFcn1(0) + fitFcn2(1)", 0.3, 0.7);
-    fitFcn3->SetParameter(0, fitFcn1->GetParameter(0));
-    fitFcn3->SetParameter(1, fitFcn2->GetParameter(1));
+    // 'gaus(0)' is a substitute for: [0]*exp(-0.5*((x-[1])/[2])**2) and (0) means start numbering parameters at 0
+    // 'expo(3)' is a substitute for: exp([3]+[4]*x)
+    // 'pol1(3)' is a substitute for: [3]+[4]*x ??need to check
+    // std::unique_ptr<TF1> fitFcn3 = std::make_unique<TF1>("fitFcn3", "gaus(0)*expo(3)", 0.3, 0.7);
+    std::unique_ptr<TF1> fitFcn3 = std::make_unique<TF1>("fitFcn3", "gaus(0)*pol1(3)", 0.3, 0.7);
+    fitFcn3->SetParameter(0, 1);
+    fitFcn3->SetParameter(1, 1);
+    fitFcn3->SetParameter(2, 1);
+    fitFcn3->SetParameter(3, 0.1);
+    fitFcn3->SetParameter(4, 1);
     fitFcn3->SetLineColor(kRed);
     h1->Fit(fitFcn3.get());
 
@@ -117,14 +123,14 @@ void ks_flat_bx2_fits() {
     legend1->AddEntry("h1", "ks_m", "l");
     // legend1->AddEntry(fitFcn1.get(), "fit: gaus", "l");
     // legend1->AddEntry(fitFcn2.get(), "fit: pol1", "l");
-    legend1->AddEntry(fitFcn3.get(), "fit: gaus + pol1", "l");
+    legend1->AddEntry(fitFcn3.get(), "fit: gaus*expo", "l");
     legend1->Draw();
 
     c1->Update();
-    c1->SaveAs("plots/ks_m_fitGauss.png");
+    c1->SaveAs("plots/f1_m_fit.png");
 }
 
 int main() {
-    ks_flat_bx2_fits();
+    f1_flat_bx2_analysis();
     return 0;
 }
