fd35d88c55d71ca6e44b2286837c2595d54dc3e0
Daniel Barton <dbart013@odu.edu>
2023-09-13 10:13:20 -0400

    change bw & bkg to 'GetParameter' from fitCombined

--- f1_flat_bx2_analysis.C
+++ f1_flat_bx2_analysis.C
@@ -102,13 +102,16 @@ void f1_flat_bx2_analysis() {
     // Then send to tyler.
 
     std::unique_ptr<TF1> bkg = std::make_unique<TF1>("bkg", "TMath::Exp([6] + [7] * x + [8] * x * x)", 1.2, 1.7);
-    std::unique_ptr<TF1> bw1420 = std::make_unique<TF1>("bw1420", "TMath::BreitWigner(x, [4], [5])", 1.2, 1.7);
-    std::unique_ptr<TF1> fitCombined = std::make_unique<TF1>("fitCombined", "bkg + bw1420", 1.2, 1.7);
-    fitCombined->FixParameter(4, 1.42); // mass
-    fitCombined->FixParameter(5, 0.05); // width
-    fitCombined->SetParameter(6, -6.47); // ?
-    fitCombined->SetParameter(7, 9.29); // ?
-    fitCombined->SetParameter(8, -2.97); // ?
+    // Function directly below (bw1420) may be defined wrong here because parameters don't print out in terminal.  trying my old bw function (aka "breitwitner(0)")
+    // std::unique_ptr<TF1> bw1420 = std::make_unique<TF1>("bw1420", "TMath::BreitWigner(x, [4], [5])", 1.2, 1.7);
+    std::unique_ptr<TF1> bw = std::make_unique<TF1>("bw", "breitwigner(0)", 1.2, 1.7);
+    std::unique_ptr<TF1> fitCombined = std::make_unique<TF1>("fitCombined", "bkg + breitwigner(0)", 1.2, 1.7);
+    fitCombined->SetParameter(0, 1.609E2); // bw amplitude
+    fitCombined->SetParameter(1, 1.427E0); // bw mass
+    fitCombined->SetParameter(2, 7.082E-2); // bw width
+    fitCombined->SetParameter(6, -6.089E1); // ?
+    fitCombined->SetParameter(7, 8.527E1); // ?
+    fitCombined->SetParameter(8, -2.670E1); // ?
     fitCombined->SetLineColor(kMagenta);
     fitCombined->SetLineWidth(2);
     fitCombined->SetLineStyle(2);
@@ -117,9 +120,28 @@ void f1_flat_bx2_analysis() {
     // fitCombined->SetParName(6, "expPar1");
     // fitCombined->SetParName(7, "expPar2");
     // fitCombined->SetParName(8, "expPar3");
-    h1->Fit(bw1420.get(), "RV");
+    // h1->Fit(bw1420.get(), "RV");
     h1->Fit(fitCombined.get(), "RV");
 
+    // Set parameters of bw and bkg functions to those of fitCombined.
+    // The arguments in 'GetParemeter(arg)' are the INDICES of the parameters in the fitCombined function, not the parameter numbers themselves.
+    // These INDICES are the same as the order in which the parameters are listed below the fitCombined function (0, 1, 2, etc.).
+    // But they are NOT the 'parameter numbers' (i.e. 1, 2, 6, 7, 8), as stated earlier.
+
+    bw->SetParameter(0, fitCombined->GetParameter(0)); // 
+    bw->SetParameter(1, fitCombined->GetParameter(1)); // 
+    bw->SetParameter(2, fitCombined->GetParameter(2)); // 
+    bw->SetLineColor(kBlue);
+    bw->SetLineWidth(2);
+    bw->SetLineStyle(2);
+
+    bkg->SetParameter(6, fitCombined->GetParameter(3)); // 
+    bkg->SetParameter(7, fitCombined->GetParameter(4)); // 
+    bkg->SetParameter(8, fitCombined->GetParameter(5)); // 
+    bkg->SetLineColor(kCyan);
+    bkg->SetLineWidth(2);
+    bkg->SetLineStyle(2);
+    
     // ******** PLOTTING ********
 
     std::shared_ptr<TCanvas> c1 = std::make_shared<TCanvas>("c1", "f1_m_fit", 800, 600);
@@ -127,15 +149,17 @@ void f1_flat_bx2_analysis() {
     // h1->GetXaxis()->SetRangeUser(xMin,xMax);
     // h1->GetYaxis()->SetRangeUser(yMin,yMax);
     h1->Draw("E"); // "E"
-    // bkg->Draw("same");
-    bw1420->Draw("same");
+    bkg->Draw("same");
+    // bw1420->Draw("same");
+    bw->Draw("same");
     fitCombined->Draw("same");
     
     auto legend1 = new TLegend(0.75, 0.77, .98, 0.58); //(x_topLeft, y_topLeft, x_bottomRight, y_bottomRight)
     legend1->AddEntry("h1", "Data: ks_m", "l");
     legend1->AddEntry(bkg.get(), "fcn: bkg", "l");
-    legend1->AddEntry(bw1420.get(), "fcn: bw1420", "l");
-    legend1->AddEntry(fitCombined.get(), "fcn: fitCombined", "l");
+    legend1->AddEntry(bw.get(), "fcn: bw", "l");
+    // legend1->AddEntry(bw1420.get(), "fcn: bw1420", "l");
+    legend1->AddEntry(fitCombined.get(), "bkg + breitwigner(0)", "l");
     legend1->Draw();
 
     c1->Update();
