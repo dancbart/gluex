b871ef42db24210aa8c2d316ccce3e57436a8661
Daniel Barton <dbart013@odu.edu>
2023-10-30 12:11:28 -0400

    voigtian fit in-prog.  this compiles bt dnt plot

--- analysis/f1_flat_bx2_analysis.C
+++ analysis/f1_flat_bx2_analysis.C
@@ -83,11 +83,9 @@ void f1_flat_bx2_analysis() {
                      .Filter(reject_delta)
                      .Filter(reject_lambda);
     
-    
-    
     // ********** HISTOGRAMS **********
     
-    auto h1 = cut_df.Filter(keep_kstar_plus).Filter(reject_kstar_zero).Histo1D({"h1", "f1_m (select charged K*, reject neutral K*)", 60, 1.2, 1.7}, "f1_m");
+    auto h1 = cut_df.Filter(reject_kstar_plus).Filter(keep_kstar_zero).Histo1D({"h1", "f1_m (reject charged K*, select neutral K*)", 60, 1.2, 1.7}, "f1_m");
     h1->SetLineColor(kBlack);
     //auto h2 = cut_df.Filter(keep_kstar_plus).Filter(keep_kstar_zero).Histo1D({"h2", "f1", 60, 1.1, 1.7}, "f1_m");
     // auto xMin = 1.0;
@@ -98,40 +96,76 @@ void f1_flat_bx2_analysis() {
 
     // ********** FITTING **********
 
-    std::unique_ptr<TF1> bkg = std::make_unique<TF1>("bkg", "TMath::Exp([0] + [1] * x + [2] * x * x)", 1.2, 1.7);
-    bkg->SetParName(0, "bkg_expPar1");
-    bkg->SetParName(1, "bkg_expPar2");
-    bkg->SetParName(2, "bkg_expPar3");
-    std::unique_ptr<TF1> bw1420 = std::make_unique<TF1>("bw1420", "breitwigner(0)", 1.2, 1.7); // used to have BreitWigner(x, [4], [5])
-    bw1420->SetParName(0, "bw1420_amplitude");
-    bw1420->SetParName(1, "bw1420_mass");
-    bw1420->SetParName(2, "bw1420_width");
-    std::unique_ptr<TF1> fitCombined = std::make_unique<TF1>("fitCombined", "bkg + bw1420", 1.2, 1.7);
-    fitCombined->SetParameter("bkg_expPar1", -6.47E0); //  -6.47E0);
-    fitCombined->SetParameter("bkg_expPar2", 9.29E0); //  9.29E0);
-    fitCombined->SetParameter("bkg_expPar3", -2.970E0); // -2.970E0);
-    fitCombined->SetParameter("bw1420_amplitude", 145); // 1.609E2
-    fitCombined->SetParameter("bw1420_mass", 1.42); // 1.420E0
-    fitCombined->SetParameter("bw1420_width", 7.082E-2); // 7.082E-2
+    // std::unique_ptr<TF1> bkg = std::make_unique<TF1>("bkg", "TMath::Exp([0] + [1] * x + [2] * x * x)", 1.2, 1.7);
+    // bkg->SetParName(0, "bkg_expPar1");
+    // bkg->SetParName(1, "bkg_expPar2");
+    // bkg->SetParName(2, "bkg_expPar3");
+    // std::unique_ptr<TF1> bw1420 = std::make_unique<TF1>("bw1420", "breitwigner(0)", 1.2, 1.7); // used to have BreitWigner(x, [4], [5])
+    // bw1420->SetParName(0, "bw1420_amplitude");
+    // bw1420->SetParName(1, "bw1420_mass");
+    // bw1420->SetParName(2, "bw1420_width");
+    std:unique_ptr<TF1> voigtian = std::make_unique<TF1>("voigtian", "TMath::Voigt(x - [0], [1], [2])", 1.2, 1.7);
+    voigtian->SetParName(0, "voigtian_mean"); // median?
+    voigtian->SetParName(1, "voigtian_sigma"); // 'sigma' is the bw width
+    voigtian->SetParName(2, "voigtian_width"); // 'lg' aka "width" is the Gaussian width (detector resolution)
+
+    // std::unique_ptr<TF1> RooVoigtian = std::make_unique<TF1>("RooVoigtian", "ROOT::RooVoigtian(x, [0], [1], [2])", 1.2, 1.7); // 'doFast use the faster look-up-table-based method for the evaluation of the complex error function.
+    // RooVoigtian->SetParName(0, "RooVoigtian_mean");
+    // RooVoigtian->SetParName(1, "RooVoigtian_width");
+    // RooVoigtian->SetParName(2, "RooVoigtian_sigma");
+
+    // Combine functions: //
+
+    std::unique_ptr<TF1> fitCombined = std::make_unique<TF1>("fitCombined", "voigtian", 1.2, 1.7);
+    // fitCombined->SetParameter("bkg_expPar1", -6.47E0); //  -6.47E0);
+    // fitCombined->SetParameter("bkg_expPar2", 9.29E0); //  9.29E0);
+    // fitCombined->SetParameter("bkg_expPar3", -2.970E0); // -2.970E0);
+    
+    // fitCombined->SetParameter("bw1420_amplitude", 145); // 1.609E2
+    // fitCombined->SetParameter("bw1420_mass", 1.42); // 1.420E0
+    // fitCombined->SetParameter("bw1420_width", 7.082E-2); // 7.082E-2
+    
+    fitCombined->SetParameter("voigtian_mean", 1.4E0); // 
+    fitCombined->SetParameter("voigtian_sigma", 2.000E0); // 
+    fitCombined->SetParameter("voigtian_width", -4.998E3); // 
+    
+    // fitCombined->SetParameter("RooVoigtian_mean", 1.285); // 1.420E0
+    // fitCombined->SetParameter("RooVoigtian_width", 0.25); // 7.082E-2
+    // fitCombined->SetParameter("RooVoigtian_sigma", 0.025); // 7.082E-2
+    
     fitCombined->SetLineColor(kMagenta);
     fitCombined->SetLineWidth(2);
     fitCombined->SetLineStyle(4);
     h1->Fit(fitCombined.get(), "RV");
 
 
-    bkg->SetParameter(0, fitCombined->GetParameter("bkg_expPar1")); // 
-    bkg->SetParameter(1, fitCombined->GetParameter("bkg_expPar2")); // 
-    bkg->SetParameter(2, fitCombined->GetParameter("bkg_expPar3")); // 
-    bkg->SetLineColor(kCyan);
-    bkg->SetLineWidth(2);
-    bkg->SetLineStyle(2);
+    // bkg->SetParameter(0, fitCombined->GetParameter("bkg_expPar1")); // 
+    // bkg->SetParameter(1, fitCombined->GetParameter("bkg_expPar2")); // 
+    // bkg->SetParameter(2, fitCombined->GetParameter("bkg_expPar3")); // 
+    // bkg->SetLineColor(kCyan);
+    // bkg->SetLineWidth(2);
+    // bkg->SetLineStyle(2);
 
-    bw1420->SetParameter(0, fitCombined->GetParameter("bw1420_amplitude")); //
-    bw1420->SetParameter(1, fitCombined->GetParameter("bw1420_mass")); //
-    bw1420->SetParameter(2, fitCombined->GetParameter("bw1420_width")); //
-    bw1420->SetLineColor(kGreen);
-    bw1420->SetLineWidth(2);
-    bw1420->SetLineStyle(2);
+    // bw1420->SetParameter(0, fitCombined->GetParameter("bw1420_amplitude")); //
+    // bw1420->SetParameter(1, fitCombined->GetParameter("bw1420_mass")); //
+    // bw1420->SetParameter(2, fitCombined->GetParameter("bw1420_width")); //
+    // bw1420->SetLineColor(kGreen);
+    // bw1420->SetLineWidth(2);
+    // bw1420->SetLineStyle(2);
+
+    // RooVoigtian->SetParameter(0, fitCombined->GetParameter("RooVoigtian_mean")); //
+    // RooVoigtian->SetParameter(1, fitCombined->GetParameter("RooVoigtian_width")); //
+    // RooVoigtian->SetParameter(2, fitCombined->GetParameter("RooVoigtian_sigma")); //
+    // RooVoigtian->SetLineColor(kGreen);
+    // RooVoigtian->SetLineWidth(2);
+    // RooVoigtian->SetLineStyle(2);
+
+    voigtian->SetParameter(0, fitCombined->GetParameter("voigtian_mean")); //
+    voigtian->SetParameter(1, fitCombined->GetParameter("voigtian_sigma")); //
+    voigtian->SetParameter(2, fitCombined->GetParameter("voigtian_width")); //
+    voigtian->SetLineColor(kGreen);
+    voigtian->SetLineWidth(2);
+    voigtian->SetLineStyle(2);
 
     
     // ******** PLOTTING ********
@@ -141,15 +175,19 @@ void f1_flat_bx2_analysis() {
     // h1->GetXaxis()->SetRangeUser(xMin,xMax);
     // h1->GetYaxis()->SetRangeUser(yMin,yMax);
     h1->Draw("E"); // "E"
-    bkg->Draw("same");
-    bw1420->Draw("same");
+    // bkg->Draw("same");
+    // bw1420->Draw("same");
+    voigtian->Draw("same");
+    // RooVoigtian->Draw("same");
     fitCombined->Draw("same");
     
     auto legend1 = new TLegend(0.75, 0.77, .98, 0.58); //(x_topLeft, y_topLeft, x_bottomRight, y_bottomRight)
     legend1->AddEntry("h1", "Data: ks_m", "l");
-    legend1->AddEntry(bkg.get(), "fcn: bkg", "l");
-    legend1->AddEntry(bw1420.get(), "fcn: bw1420", "l");
-    legend1->AddEntry(fitCombined.get(), "bkg + bw1420", "l");
+    // legend1->AddEntry(bkg.get(), "fcn: bkg", "l");
+    // legend1->AddEntry(bw1420.get(), "fcn: bw1420", "l");
+    // legend1->AddEntry(RooVoigtian.get(), "fcn: RooVoigtian", "l");
+    legend1->AddEntry(voigtian.get(), "fcn: voigtian", "l");
+    legend1->AddEntry(fitCombined.get(), "bkg + voigtan", "l");
     legend1->Draw();
 
     c1->Update();
