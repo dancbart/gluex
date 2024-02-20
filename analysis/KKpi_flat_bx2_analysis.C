6d7339b468fa62367838adea7f8e2fa624bada3a
Daniel Barton <dbart013@odu.edu>
2024-02-19 23:37:45 -0500

    updated cuts, updated histos for Gluex Winter presentation

--- analysis/KKpi_flat_bx2_analysis.C
+++ analysis/KKpi_flat_bx2_analysis.C
@@ -95,19 +95,27 @@ void analysis(double f1_mMin, double f1_mMax, int plotIndex) {
     auto reject_kstar_zero = "kmpip1_m <= 0.8 || kmpip1_m >= 1.0"; // aka "neutral" K*(K-Pi+)
     auto select_kShort = "ks_m >= 0.45 && ks_m <= 0.55";
     auto reject_kShort = "ks_m <= 0.45 || ks_m >= 0.55";
-    // Select f1(1420) mass range
-    // auto select_f1 = "f1_m >= f1_mMin && f1_m <= f1_mMax";
-    auto select_f1 = [=](double f1_m) { return f1_m >= f1_mMin && f1_m <= f1_mMax; };
+    auto select_f1 = [=](double f1_m) { return f1_m >= f1_mMin && f1_m <= f1_mMax; }; // lambda function to select f1 mass range
 
     // Apply cuts; make new dataframe
-    auto cut_df = df2.Filter(reject_delta)
+
+    // Old cuts (~5/2023).  Used when originally looking for f1(1420) resonance
+    auto cut_df0 = df2.Filter("pkm_m > 1.9") // lambda cut
+                     .Filter("pip1p_m > 1.4") // delta++ cut
+                     .Filter("pathlength_sig > 5");
+                    //  Not sure why these kspip1 and kmpip1 cuts were here.  They don't make much sense, and also cut out almost all the data.
+                    //  .Filter("kspip1_m > 1.1") // kspip1 cut
+                    //  .Filter("kmpip1_m > 1.1") // kmpip1 cut
+
+    // Working cuts (2/2024).  Use these for active analysis
+    auto cut_df1 = df2.Filter(reject_delta)
                      .Filter(reject_lambda)
                      .Filter(select_f1, {"f1_m"})
                      .Filter("pathlength_sig > 5");
                     //  .Filter(select_kShort);
 
     // Other cuts (in case I want two histograms on the same canvas, for example)
-    auto cut_df_alternate = df2.Filter(reject_delta)
+    auto cut_df2 = df2.Filter(reject_delta)
                      .Filter(reject_lambda)
                      .Filter(select_f1, {"f1_m"});
                     //  .Filter("pathlength_sig > 5");
@@ -122,20 +130,24 @@ void analysis(double f1_mMin, double f1_mMax, int plotIndex) {
     // auto yMin = 0;
     // auto yMax = 15000;
 
-    // 1D histogram of M(pip2 + pim) (mass of Ks).
-    auto h1 = cut_df.Histo1D({"hNew", Form("M(pip2 + pim).  Bins of: pipKmKs invariant mass (GeV): %.2f - %.2f", f1_mMin, f1_mMax), 60, 0.400, 0.600}, "ks_m");
-    h1->SetLineColor(kRed);
-    // 1D histogram of M(pip2 + pim) (mass of Ks). DIFFERENT CUTS!
-    // auto h2 = cut_df_alternate.Histo1D({"hNew", Form("M(pip2 + pim).  Bins of: pipKmKs invariant mass (GeV): %.2f - %.2f", f1_mMin, f1_mMax), 60, 0.400, 0.600}, "ks_m");
-    // h2->SetLineColor(kBlue);
+    // 1D Histogram of KKpi full mass range (to see where the f1(1420) is)
+    auto h1 = cut_df1.Histo1D({"h1", Form("Bin of KKpi invariant mass (GeV): %.2f - %.2f", f1_mMin, f1_mMax), 60, 1.2, 1.7}, "f1_m");
+    h1->SetLineColor(kBlue);
+
+    // // 1D histogram of M(pip2 + pim) (mass of Ks).
+    // auto h1 = cut_df.Histo1D({"hNew", Form("M(pip2 + pim).  Bins of: pipKmKs invariant mass (GeV): %.2f - %.2f", f1_mMin, f1_mMax), 60, 0.400, 0.600}, "ks_m");
+    // h1->SetLineColor(kRed);
+    // // 1D histogram of M(pip2 + pim) (mass of Ks). DIFFERENT CUTS!
+    // // auto h2 = cut_df_alternate.Histo1D({"hNew", Form("M(pip2 + pim).  Bins of: pipKmKs invariant mass (GeV): %.2f - %.2f", f1_mMin, f1_mMax), 60, 0.400, 0.600}, "ks_m");
+    // // h2->SetLineColor(kBlue);
 
     // ********** DALITZ PLOTS ********** (i.e. 2D HISTOGRAMS)
 
-    // // auto h3 = cut_df.Histo2D({"h3", "Select f1: 1.3 - 1.5)", 200, 0.6, 1.1, 200, 0.6, 1.1}, "kspip1_m", "kmpip1_m");
-    // // create histogram where the histogram label is a string that is a function of the f1 mass range
-    // auto h3 = cut_df.Histo2D({"h2", Form("Bins of: pipKmKs invariant mass (GeV): %.2f - %.2f", f1_mMin, f1_mMax), 40, 0.100, 0.200, 40, 0.100, 0.200}, "pip2_m2", "pim_m2");
-    // // auto h3 = cut_df.Histo2D({"h3", "", 200, 0.6, 1.1, 200, 0.6, 1.1}, "kspip1_m", "kmpip1_m");
-    // h3->SetLineColor(kGreen);
+    // // kShort Dalitz plot
+    // auto h2 = cut_df.Histo2D({"h2", Form("Bins of: pipKmKs invariant mass (GeV): %.2f - %.2f", f1_mMin, f1_mMax), 40, 0.100, 0.200, 40, 0.100, 0.200}, "pip2_m2", "pim_m2");
+    // // K* Dalitz plot
+    // auto h3 = cut_df.Histo2D({"h3", Form("Bins of: KKpi invariant mass (GeV): %.2f - %.2f", f1_mMin, f1_mMax), 40, 0.1, 1.4, 40, 0.1, 1.4}, "kspip1_m2", "kmpip1_m2");
+    // h3->SetLineColor(kRed);
 
 
     // ********** FITTING **********
@@ -239,22 +251,47 @@ void analysis(double f1_mMin, double f1_mMax, int plotIndex) {
     
     // ******** PLOTTING ********
 
-    std::shared_ptr<TCanvas> c1 = std::make_shared<TCanvas>("c1", "M(pi+pi-)", 800, 600);
+    std::shared_ptr<TCanvas> c1 = std::make_shared<TCanvas>("c1", "Canvas", 800, 600);
+
+    // options to draw histogram with are: "E" (error bars), "H" (histogram), "L" (line), "P" (markers), "C" (curve), "B" (bar chart), "A" (area), "9" (same as "H" but fills with a color), "hist" (same as "H"), "histc" (same as "C"), "same" (superimpose on previous picture), "nostack" (don't stack bars), "nol" (don't draw the line), "noc" (don't draw the markers), "nofunction" (don't draw the function), "text" (draw bin contents as text), "goff" (graphics off), "e1" (draw error bars only), "e2" (draw error rectangles only), "e3" (draw error bars and rectangles), "e4" (draw a fill area through the end points of the vertical error bars), "e5" (draw a smooth fill area through the end points of the vertical error bars), "e6" (draw a smooth fill area through the end points of the error bars), "e7" (draw a fill area through the end points of the error bars)
+    // and options to draw the dalitz plot (h2) with are: "COLZ" (draw a color plot representing the cell contents), "CONTZ" (draw a contour plot representing the cell contents), "LEGO" (draw a lego plot representing the cell contents), "SURF" (draw a surface plot representing the cell contents), "SURF1" (draw a surface plot representing the cell contents, with hidden line removal), "SURF2" (draw a surface plot representing the cell contents, with color representation of the cell contents), "SURF3" (draw a surface plot representing the cell contents, with color representation of the cell contents and hidden line removal), "SURF4" (draw a surface plot representing the cell contents, with Gouraud shading), "SURF5" (draw a surface plot representing the cell contents, with color representation of the cell contents and Gouraud shading), "SURF6" (draw a surface plot representing the cell contents, with color representation of the cell contents, Gouraud shading and hidden line removal)
 
     // h1->GetXaxis()->SetRangeUser(xMin,xMax);
     // h1->GetYaxis()->SetRangeUser(yMin,yMax);
     // h1->Draw("E"); // "E"
-    // draw the histogram as a histogram instead of data points
+    // draw as a histogram instead of data points
     h1->Draw("HIST");
+    // Usage: auto arrow = new TArrow(x_position, y_start, x_position, y_end, 0.02, "|>"); // Adjust x_position, y_start, y_end as needed
+    // auto arrow1 = new TArrow(1.295, 4000, 1.295, 2600, 0.01, "|>"); // Example positions
+    // arrow1->SetLineWidth(8);
+    // arrow1->Draw();
+    // arrow1->SetLineColor(kRed);
+    // auto latex1 = new TLatex(1.295, 4200, "1.295 GeV"); // Adjust position and LaTeX formatted text as needed
+    // latex1->SetTextAlign(22); // Centers the text horizontally and vertically
+    // latex1->SetTextSize(0.03); // Adjust text size as needed
+    // latex1->Draw();
+
+    auto arrow2 = new TArrow(1.420, 3000, 1.420, 4000, 0.01, "|>"); // Example positions
+    arrow2->SetLineWidth(8);
+    arrow2->Draw();
+    arrow2->SetLineColor(kRed);
+    auto latex2 = new TLatex(1.420, 2800, "1.420 GeV"); // Adjust position and LaTeX formatted text as needed
+    latex2->SetTextAlign(22); // Centers the text horizontally and vertically
+    latex2->SetTextSize(0.03); // Adjust text size as needed
+    latex2->Draw();
+
+
+    // // Draw histogram for pip2 vs. pim mass squared (evidence of Ks)
     // h2->Draw("same");
-    // options to draw histogram with are: "E" (error bars), "H" (histogram), "L" (line), "P" (markers), "C" (curve), "B" (bar chart), "A" (area), "9" (same as "H" but fills with a color), "hist" (same as "H"), "histc" (same as "C"), "same" (superimpose on previous picture), "nostack" (don't stack bars), "nol" (don't draw the line), "noc" (don't draw the markers), "nofunction" (don't draw the function), "text" (draw bin contents as text), "goff" (graphics off), "e1" (draw error bars only), "e2" (draw error rectangles only), "e3" (draw error bars and rectangles), "e4" (draw a fill area through the end points of the vertical error bars), "e5" (draw a smooth fill area through the end points of the vertical error bars), "e6" (draw a smooth fill area through the end points of the error bars), "e7" (draw a fill area through the end points of the error bars)
-    // and options to draw the dalitz plot (h2) with are: "COLZ" (draw a color plot representing the cell contents), "CONTZ" (draw a contour plot representing the cell contents), "LEGO" (draw a lego plot representing the cell contents), "SURF" (draw a surface plot representing the cell contents), "SURF1" (draw a surface plot representing the cell contents, with hidden line removal), "SURF2" (draw a surface plot representing the cell contents, with color representation of the cell contents), "SURF3" (draw a surface plot representing the cell contents, with color representation of the cell contents and hidden line removal), "SURF4" (draw a surface plot representing the cell contents, with Gouraud shading), "SURF5" (draw a surface plot representing the cell contents, with color representation of the cell contents and Gouraud shading), "SURF6" (draw a surface plot representing the cell contents, with color representation of the cell contents, Gouraud shading and hidden line removal)
     // h2->Draw("COLZ");
-    // axes labels
-    // h2->GetXaxis()->SetTitle("K_{S}#pi^{+} mass Squared (GeV^{2})");
-    // h2->GetYaxis()->SetTitle("K^{-}#pi^{+} mass Squared (GeV^{2})");
     // h2->GetXaxis()->SetTitle("#pi^{+} mass Squared (GeV^{2})");
     // h2->GetYaxis()->SetTitle("#pi^{-} mass Squared (GeV^{2})");
+
+    // // Draw histogram for (Ks + pip1) vs. (Km + pip1) mass squared (evidence of f1(1420))
+    // h3->Draw("COLZ");
+    // h3->GetXaxis()->SetTitle("K_{S}#pi^{+} mass squared (GeV^{2})");
+    // h3->GetYaxis()->SetTitle("K^{-}#pi^{+} mass squared (GeV^{2})");
+    
     // bkg->Draw("same");
     // bw1420->Draw("same");
     // voigtian->Draw("same");
@@ -262,8 +299,8 @@ void analysis(double f1_mMin, double f1_mMax, int plotIndex) {
     // fitCombined->Draw("same");
 
 
-    auto legend1 = new TLegend(0.75, 0.77, .98, 0.58); //(x_topLeft, y_topLeft, x_bottomRight, y_bottomRight)
-    legend1->AddEntry(h1.GetPtr(), "Pathlength_sig > 5", "l"); // using "GetPtr()" instead of "get()" because "get()" is a method of unique_ptr, not shared_ptr, something like that.
+    // auto legend1 = new TLegend(0.75, 0.77, .98, 0.58); //(x_topLeft, y_topLeft, x_bottomRight, y_bottomRight)
+    // legend1->AddEntry(h3.GetPtr(), "Pathlength_sig > 5", "l"); // using "GetPtr()" instead of "get()" because "get()" is a method of unique_ptr, not shared_ptr, something like that.
     // legend1->AddEntry(h2.GetPtr(), "No pathlength cut", "l");
     
     // legend1->AddEntry("h1", "Data, no fitting", "l");
@@ -276,10 +313,11 @@ void analysis(double f1_mMin, double f1_mMax, int plotIndex) {
     // legend1->AddEntry(voigtian.get(), "fcn: voigtian", "l");
     // legend1->AddEntry(voigtian2.get(), "fcn: voigtian2", "l");
     // legend1->AddEntry(fitCombined.get(), "bkg + voigtan", "l");
-    legend1->Draw();
+    // legend1->Draw();
     // legend2->Draw();
 
-    TString plotName = Form("../plots/kShort/M(pip2 + pim)_%d_pathlength_sig.png", plotIndex); // plotIndex is the index of the KKpi mass range and is set in the for loop in the main function
+    // TString plotName = Form("../plots/dalitzPlots/kMinusPip_Vs_kShortPip_SQUARED_%d.png", plotIndex); // plotIndex is the index of the KKpi mass range and is set in the for loop in the main function
+    TString plotName = Form("../_plots/KKpi_findingResonance_1420GeV.png"); // plotIndex is the index of the KKpi mass range and is set in the for loop in the main function
     c1->Update();
     c1->SaveAs(plotName);
     // c2->Update();
@@ -307,10 +345,10 @@ void analysis(double f1_mMin, double f1_mMax, int plotIndex) {
 void KKpi_flat_bx2_analysis() {
     // For loop running through "analysis" for different ranges of f1 masses
     // create 2D array of masses from 1.1 to 2.0 GeV in 100 MeV steps
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 1; i++) {
         int plotIndex = i;
-        double f1_mMin = 1.1 + i * 0.1;
-        double f1_mMax = 1.2 + i * 0.1;
+        double f1_mMin = 1.2 + i * 0.05;
+        double f1_mMax = 1.70 + i * 0.05;
         analysis(f1_mMin, f1_mMax, plotIndex);
     }
 
